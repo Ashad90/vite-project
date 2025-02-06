99663c0b2e95ffc1773c9234fbc4f5061fe58a00
Redemona Christ <redemonachrist@gmail.com>
2025-02-06 14:43:04 +0100

    first commit

---
+++ src/01/App01.tsx
@@ -0,0 +1,16 @@
+import './style.css'
+import Logo from './logo.webp'
+
+
+function App01 () {
+    return (
+        <div className="text">
+            <div>
+            <img src={Logo} alt="Logo" width={500} />
+            </div>
+            Mon premier composant React
+        </div>
+    )
+}
+
+export default App01;
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+import './01/style.css'
+// import App from './App.tsx'
+import App01 from './01/App01.tsx'
+
+
+createRoot(document.getElementById('root')!).render(
+    <App01 /> 
+)
